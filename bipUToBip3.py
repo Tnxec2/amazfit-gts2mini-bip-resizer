7a1fdf2d6766faf03c6d3c2605763c0dfeb06ad1
Nikolas Maletschkin <tnxec2@gmail.com>
2023-05-19 13:47:35 +0200

    start scripts for any devices

---
+++ bipUToBip3.py
@@ -0,0 +1,25 @@
+
+
+#!/usr/bin/python
+
+from resizer import Resizer
+
+
+
+# gts2mini: 306 x 354
+# bipU: 302 x 320
+# bip3: 240 x 280
+
+scaleGTS2MINItoBIP3 = (240 / 306, 280 / 354)
+scaleBIPUtoBIP3 = (240 / 302, 280 / 320)
+scaleGTS2MINItoBIPU = (302 / 306,  320 / 354)
+
+
+outputDirName = 'bip3'
+
+resizer = Resizer(outputDirName, scaleBIPUtoBIP3)
+resizer.resize()
+    
+
+
+
---
+++ gts2MiniToBipU.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from resizer import Resizer
+
+
+# gts2mini: 306 x 354
+# bipU: 302 x 320
+# bip3: 240 x 280
+
+scaleGTS2MINItoBIP3 = (240 / 306, 280 / 354)
+scaleBIPUtoBIP3 = (240 / 302, 280 / 320)
+scaleGTS2MINItoBIPU = (302 / 306,  320 / 354)
+
+
+outputDirName = 'bipU'
+
+resizer = Resizer(outputDirName, scaleGTS2MINItoBIPU)
+resizer.resize()
